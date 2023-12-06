7105906567bf47f224c7c0770ece1b3552d84a24
gwongibeom <k201gun@kakao.com>
2023-12-07 01:02:39 +0900

    (개발)cors 업데이트

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import express from 'express'
 import fs from 'fs/promises'
+import cors from 'cors'
 
 const app = express()
+app.use(cors())
 const PORT = process.env.PORT || 3000
 const jsonFilePath = './techstack_names.json'
 
